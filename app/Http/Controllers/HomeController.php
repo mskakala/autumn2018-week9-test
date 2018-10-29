28a3d8464ecc567b497fa6edf6976573c8ebd043
MS <martin.skakala@gmail.com>
2018-10-29 12:12:26 +0100

    Q10

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Hero;
 
 class HomeController extends Controller
 {
@@ -23,7 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $results = Hero::all();
+        //->orderBy('name');
+        //->get();
+
+        return view('home', compact('results'));
     }
 
 //NEW
--- resources/views/hero/index.blade.php
+++ resources/views/hero/index.blade.php
@@ -5,4 +5,11 @@
 
 <h1>The hero roster</h1>
 
+
+<ol>
+@foreach($results as $result)
+<li>{{ $result->name }}</li>
+@endforeach 
+</ol>
+
 @endsection 

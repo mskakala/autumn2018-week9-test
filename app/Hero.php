276196c8392cde7f3bcce38127e0dc3ce359b9f9
MS <martin.skakala@gmail.com>
2018-10-29 12:00:40 +0100

    Q9

--- app/Hero.php
+++ app/Hero.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hero extends Model
 {
-    //
+    //Many to many relationship
+
+
+    public function images()
+    {
+        return $this->belongsToMany('App\Image','path_id', 'description_id');
+    }
+
+
+
 }
